0b97fd7de8908ff71ba95d054f0e60fb7e578002
ianers09 <ianoliver.mingoy@cit.edu>
2025-11-26 21:27:46 +0800

    Shop Page UI Fix

--- gallery.php
+++ gallery.php
@@ -28,7 +28,7 @@ $current_page = basename($_SERVER['PHP_SELF']);
 <head>
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
-<title>Builder's Corner - Shop</title>
+<title>Builder's Corner</title>
 
 <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700&display=swap" rel="stylesheet">
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
